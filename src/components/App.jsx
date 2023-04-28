0e2a8fb932002b9a8649ab572f06c1b19f509e40
ValeriiHol <rielty@gmail.com>
2023-04-28 15:17:12 +0300

    task 1,2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+
+import user from '../data/user.json';
+import data from '../data/data.json';
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +17,14 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      goit-react-hw-01-components
+ <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+/>
+<Statistics title="Upload stats" stats={data} />  
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css'
+
+
+export const Profile = (props) => {
+   
+    return (
+        <div className={css.profile}>
+        <div className={css.description}>
+        <img
+        src={props.avatar}
+        alt={props.username}
+        className={css.avatar}
+        />
+            <p className={css.name}>{props.username}</p>
+            <p className={css.tag}>@{props.tag}</p>
+            <p className={css.location}>{props.location}</p>
+        </div>
+  <ul className={css.stats}>
+    <li>
+      <span className={css.label}>Followers</span>
+      <span className={css.quantity}>{props.stats.followers}</span>
+    </li>
+    <li>
+      <span className={css.label}>Views</span>
+      <span className={css.quantity}>{props.stats.views}</span>
+    </li>
+    <li>
+      <span className={css.label}>Likes</span>
+      <span className={css.quantity}>{props.stats.likes}</span>
+    </li>
+  </ul>
+</div>
+    )
+};
+
